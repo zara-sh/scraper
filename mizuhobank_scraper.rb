3fbcc9edd3505a5fea9541f7372684895a96692b
zara-sh <zara.regist@gmail.com>
2019-01-18 11:20:53 +0900

    fix bug by converting get.chomp to intger

--- mizuhobank_scraper.rb
+++ mizuhobank_scraper.rb
@@ -5,7 +5,7 @@ require "byebug"
 
 puts "Do you want to Buy or Sell?"
 puts "Enter 1 if you want to buy, enter 2 if you want to sell"
-query = gets.chomp
+query = gets.chomp.to_i
 
 driver_mizuho = Selenium::WebDriver.for :chrome
 driver_mizuho.get"https://www.mizuhobank.co.jp/market/cash.html"
@@ -17,9 +17,9 @@ mizuho_bank_j_2_f = mizuho_bank.css(".sp-none").css("tbody tr").css("td")[1]
 mizuho_bank_f_2_j = mizuho_bank.css(".sp-none").css("tbody tr").css("td")[2]
 
 
- puts "From Japanese yen to foreign currency (111.47)  #{mizuho_bank_j_2_f.text}"
- puts "From foreign currency to Japanese yen (105.47)  #{mizuho_bank_f_2_j.text}"
- puts "-------------------------------"
+ # puts "From Japanese yen to foreign currency (111.47)  #{mizuho_bank_j_2_f.text}"
+ # puts "From foreign currency to Japanese yen (105.47)  #{mizuho_bank_f_2_j.text}"
+ # puts "-------------------------------"
 
 
 
@@ -27,38 +27,38 @@ smbc_url = open("https://www.smbc.co.jp/ex/ExchangeServlet?ScreenID=real")
 smbc_bank_doc = smbc_url.read
 smbc_parse = Nokogiri::HTML (smbc_bank_doc)
 
-puts " "
-puts " "
+# puts " "
+# puts " "
 smbc_bank_j_2_f = smbc_parse.css('td')[2]
 smbc_bank_f_2_j= smbc_parse.css('td')[1]
-puts "From Japanese yen to foreign currency #{smbc_bank_j_2_f.text}"
-puts "----------------------------------------"
-puts "From foreign currency to Japanese yen #{smbc_bank_f_2_j.text}"
+# puts "From Japanese yen to foreign currency #{smbc_bank_j_2_f.text}"
+# puts "----------------------------------------"
+# puts "From foreign currency to Japanese yen #{smbc_bank_f_2_j.text}"
 
 gpa_url = open("https://www.gpa-net.co.jp/en/passenger-service/rate/")
 gpa_doc = gpa_url.read
 parse_gpa = Nokogiri::HTML(gpa_doc)
 
-puts " "
-puts " "
+# puts " "
+# puts " "
 gpa_f_2_j = parse_gpa.css("td.table-td3")[0]
 gpa_j_2_f = parse_gpa.css("td.table-td4")[0]
 
-puts "From Japanese yen to foreign currency #{gpa_j_2_f.text}"
-puts "----------------------------------------"
-puts "From foreign currency to Japanese yen #{gpa_f_2_j.text}"
+# puts "From Japanese yen to foreign currency #{gpa_j_2_f.text}"
+# puts "----------------------------------------"
+# puts "From foreign currency to Japanese yen #{gpa_f_2_j.text}"
 
 
 mufg_url = open("https://www.bk.mufg.jp/gdocs/kinri/list_j/kinri/kawase.html#")
 mufg_doc = mufg_url.read
 parse_mufg = Nokogiri::HTML(mufg_doc)
 
-puts " "
-puts " "
+# puts " "
+# puts " "
 mufg_f_2_j = parse_mufg.css("tr.tb-bg0")[1].css("td")[8]
 mufg_j_2_f = parse_mufg.css("tr.tb-bg0")[1].css("td")[5]
-puts "From Japanese yen to foreign currency #{mufg_j_2_f.inner_text}"
-puts "From foreign currency to Japanese yen #{mufg_f_2_j.inner_text}"
+# puts "From Japanese yen to foreign currency #{mufg_j_2_f.inner_text}"
+# puts "From foreign currency to Japanese yen #{mufg_f_2_j.inner_text}"
 
 
 
@@ -72,9 +72,9 @@ buy  = j_2_f.select {|k,v|  v if v == j_2_f.values.min}
 
 
 
-if query === 1
-  puts "you can buy at #{buy.values.first} price in #{buy.keys.to_s} bank"
+if query == 1
+  puts "you can buy at #{buy.values.first} price in #{buy.keys.join.split("_")[0].strip.capitalize} bank"
 else
-  puts "you can sell at #{sell.values.first} price in #{sell.keys.to_s} bank"
+  puts "you can sell at #{sell.values.first} price in #{sell.keys.join.split("_")[0].strip.capitalize} bank"
 end
 
